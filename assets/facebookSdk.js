07dc9c921127b1b8f0893b3cee617fd4131a0db7
Jon Christensen <jon.christensen@10up.com>
2020-04-06 13:54:55 -0500

    compiled assets

--- assets/facebookSdk.js
+++ assets/facebookSdk.js
@@ -1,1 +1,96 @@
-!function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}var n={};t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=999)}({999:function(e,t,n){"use strict";if(!document.getElementById("fb-root")){var o=document.createElement("div");o.id="fb-root",document.body.appendChild(o)}document.addEventListener("DOMContentLoaded",function(){if(!document.getElementById("facebook-jssdk")){var e=document.getElementsByTagName("script")[0],t=document.createElement("script");t.id="facebook-jssdk",t.src="https://connect.facebook.net/en_US/sdk.js",e.parentNode.insertBefore(t,e)}})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1002);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 1002:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+if (!document.getElementById('fb-root')) {
+  // create div required for fb
+  var fbDiv = document.createElement('div');
+  fbDiv.id = 'fb-root';
+  document.body.appendChild(fbDiv);
+}
+
+// inject fb sdk.js
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.getElementById('facebook-jssdk')) {
+    return;
+  }
+  var fjs = document.getElementsByTagName('script')[0];
+  var js = document.createElement('script');
+  js.id = 'facebook-jssdk';
+  js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+});
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=facebookSdk.js.map?v=0a1f38
